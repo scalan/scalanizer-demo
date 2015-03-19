0e939231ff9870b2664dabd36fc9c8be1871bd1f
Maxim Gekk <gekk.maxim@huawei.com>
2015-03-19 11:43:36 +0300

    Clean up BoilerplateTool.scala

--- meta/src/main/scala/scalan/meta/BoilerplateTool.scala
+++ meta/src/main/scala/scalan/meta/BoilerplateTool.scala
@@ -1,24 +1,25 @@
 package scalan.meta
 
-object StarterBoilerplateTool extends BoilerplateTool {
-  val starterTypeSynonims = Set(
-    "My"
+object ScalanParadiseBoilerplateTool extends BoilerplateTool {
+  val paradiseTypeSynonims = Map(
     // declare your type synonims for User Defined types here (see type PA[A] = Rep[PArray[A]])
+    "" -> ""
   )
-  lazy val starterConfig = CodegenConfig(
+  lazy val paradiseConfig = CodegenConfig(
+    name = "Scalan Paradise",
     srcPath = "src/main/scala",
     entityFiles = List(
-      "scalan/examples/MyArrays.scala"
+      ""
     ),
-    seqContextTrait = "ScalanSeq",
-    stagedContextTrait = "ScalanExp",
+    baseContextTrait = "ScalanCommunityDsl",
+    seqContextTrait = "ScalanCommunityDslSeq",
+    stagedContextTrait = "ScalanCommunityDslExp",
     extraImports = List(
       "scala.reflect.runtime.universe._",
       "scalan.common.Default"),
-    starterTypeSynonims
+    paradiseTypeSynonims
   )
 
-  override def getConfigs(args: Array[String]) = Seq(starterConfig)
-
+  override def getConfigs(args: Array[String]) = Seq(paradiseConfig)
   override def main(args: Array[String]) = super.main(args)
 }
