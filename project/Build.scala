2859d220173bdcbd8640b9dc4088a2b2b76b1b80
Maxim Gekk <gekk.maxim@huawei.com>
2015-03-19 11:29:52 +0300

    Clean up Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -1,15 +1,12 @@
 import sbt._
 import sbt.Keys._
-//import sbtassembly.Plugin._
-//import AssemblyKeys._
-//import sbtrelease.ReleasePlugin._
 
 object ScalanStartRootBuild extends Build {
   val commonDeps = libraryDependencies ++= Seq(
     "org.scalatest" %% "scalatest" % "2.2.1" % "test",
     "org.scalacheck" %% "scalacheck" % "1.11.5" % "test")
 
-  val testSettings = inConfig(ItTest)(Defaults.testTasks /*++ baseAssemblySettings*/) ++ Seq(
+  val testSettings = inConfig(ItTest)(Defaults.testTasks) ++ Seq(
     testOptions in Test := Seq(Tests.Argument(TestFrameworks.JUnit, "-a", "-s"), Tests.Filter(unitFilter)),
     testOptions in ItTest := Seq(Tests.Argument(TestFrameworks.JUnit, "-v", "-a", "-s", "-q"), Tests.Filter(itFilter)),
     parallelExecution in Test := false,
@@ -27,67 +24,43 @@ object ScalanStartRootBuild extends Build {
       "-language:higherKinds",
       "-language:implicitConversions",
       "-language:existentials",
-      "-language:postfixOps"))
-
-  lazy val noPublishingSettings = Seq(
-    publishArtifact := false,
-    publish := {},
-    publishLocal := {})
+      "-language:postfixOps")
+  )
 
   override lazy val settings = super.settings ++ buildSettings
-
-  lazy val commonSettings =
-    buildSettings /*++ assemblySettings ++ releaseSettings*/ ++ testSettings ++
-      Seq(
-      resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
-      publishTo := {
-        val nexus = "http://10.122.85.37:9081/nexus/"
-        if (version.value.trim.endsWith("SNAPSHOT"))
-          Some("snapshots" at (nexus + "content/repositories/snapshots"))
-        else
-          Some("releases" at (nexus + "content/repositories/releases"))
-      },
-      commonDeps)
+  lazy val commonSettings = buildSettings ++ testSettings
 
   implicit class ProjectExt(p: Project) {
     def allConfigDependency = p % "compile->compile;test->test"
-
     def addTestConfigsAndCommonSettings =
       p.configs(ItTest).settings(commonSettings: _*)
   }
-
-  def liteProject(name: String) = ProjectRef(file("../scalan-lite"), name)
-
   def liteDependency(name: String) = "com.huawei.scalan" %% name % "0.2.7-SNAPSHOT"
 
   lazy val metaDeps = liteDependency("meta")
-  lazy val startermeta = Project(
-    id = "starter-meta",
-    base = file("meta")).addTestConfigsAndCommonSettings.
-    settings(libraryDependencies ++= Seq(metaDeps))
+  lazy val scalanParadiseMeta = Project(
+    id = "scalan-paradise-meta",
+    base = file("meta")
+  ).addTestConfigsAndCommonSettings.settings(libraryDependencies ++= Seq(metaDeps))
 
   lazy val core = liteDependency("core")
-  lazy val start = Project(
-    id = "scalan-starter",
-    base = file(".")).addTestConfigsAndCommonSettings.
-    settings(libraryDependencies ++= Seq(core, core % "test" classifier "tests"))
+  lazy val common = liteDependency("common")
+  lazy val community = liteDependency("community-edition")
 
-  def itFilter(name: String): Boolean =
-    name endsWith "ItTests"
+  lazy val scalanParadise = Project(
+    id = "scalan-paradise",
+    base = file(".")
+  ).addTestConfigsAndCommonSettings.settings(libraryDependencies ++= Seq(
+    core,
+    core % "test" classifier "tests",
+    common,
+    common % "test" classifier "tests",
+    community
+  ))
 
+  def itFilter(name: String): Boolean = name endsWith "ItTests"
   def unitFilter(name: String): Boolean = !itFilter(name)
-
   lazy val ItTest = config("it").extend(Test)
-
   publishArtifact in Test := true
-
   publishArtifact in (Test, packageDoc) := false
-
-  publishTo in ThisBuild := {
-    val nexus = "http://10.122.85.37:9081/nexus/"
-    if (version.value.trim.endsWith("SNAPSHOT"))
-      Some("snapshots" at (nexus + "content/repositories/snapshots"))
-    else
-      Some("releases" at (nexus + "content/repositories/releases"))
-  }
 }
