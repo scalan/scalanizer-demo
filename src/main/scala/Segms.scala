12d5291b0a5380fc2230d0af8d79117130514245
Maxim Gekk <gekk.maxim@huawei.com>
2015-04-21 11:22:24 +0300

    Companions is added automatically.

--- src/main/scala/Segms.scala
+++ src/main/scala/Segms.scala
@@ -8,19 +8,16 @@ trait Segms {
     def end: Int
     def shift(ofs: Int): Segm
   }
-  trait SegmCompanion
 
   class Interval(val start: Int, val end: Int) extends Segm {
     def length: Int = end - start
     def shift(ofs: Int) = new Interval(start + ofs, end + ofs)
   }
-  trait IntervalCompanion
 
   class Slice(val start: Int, val length: Int) extends Segm {
     def end: Int = start + length
     def shift(ofs: Int) = new Slice(start + ofs, length)
   }
-  trait SliceCompanion
 
   class Centered(val center: Int, val radius: Int) extends Segm {
     def start: Int = center - radius
@@ -28,5 +25,4 @@ trait Segms {
     def length: Int = radius * 2
     def shift(ofs: Int) = new Centered(center + ofs, radius)
   }
-  trait CenteredCompanion
 }
