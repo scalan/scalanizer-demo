0d349e5661e69c3ca5a97a486966c3a6b2cf915d
Maxim Gekk <gekk.maxim@huawei.com>
2015-04-24 18:22:29 +0300

    Deep dive

--- src/main/scala/Segms.scala
+++ src/main/scala/Segms.scala
@@ -10,13 +10,21 @@ trait Segms {
   }
 
   class Interval(val start: Int, val end: Int) extends Segm {
-    def length: Int = end - start
+    def length: Int = {
+      val flag: Boolean = true
+      def foo: (Int, Long, Double) = (0, 1, 2.0)
+      if (flag) {
+        val diff: Int = end - start
+        diff: Int
+      } else foo._1
+    }
     def shift(ofs: Int) = new Interval(start + ofs, end + ofs)
   }
 
   class Slice(val start: Int, val length: Int) extends Segm {
     def end: Int = start + length
-    def shift(ofs: Int) = new Slice(start + ofs, length)
+    def shift(ofs: Int) = shiftBy((x: Int) => x + ofs)
+    def shiftBy(f: Int => Int) = new Slice(f(start), length)
   }
 
   class Centered(val center: Int, val radius: Int) extends Segm {
