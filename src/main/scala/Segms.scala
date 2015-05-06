8ae65a6a7a1bf0e9b2fafc541ac546b539f82a7f
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-06 16:54:35 +0300

    classes are replaced by case classes

--- src/main/scala/Segms.scala
+++ src/main/scala/Segms.scala
@@ -9,20 +9,20 @@ trait Segms {
     def shift(ofs: Int): Segm
   }
 
-  class Interval(val start: Int, val end: Int) extends Segm {
+  case class Interval(val start: Int, val end: Int) extends Segm {
     def length: Int = end - start
-    def shift(ofs: Int) = new Interval(start + ofs, end + ofs)
+    def shift(ofs: Int) = Interval(start + ofs, end + ofs)
   }
 
-  class Slice(val start: Int, val length: Int) extends Segm {
+  case class Slice(val start: Int, val length: Int) extends Segm {
     def end: Int = start + length
-    def shift(ofs: Int) = new Slice(start + ofs, length)
+    def shift(ofs: Int) = Slice(start + ofs, length)
   }
 
-  class Centered(val center: Int, val radius: Int) extends Segm {
+  case class Centered(val center: Int, val radius: Int) extends Segm {
     def start: Int = center - radius
     def end: Int = center + radius
     def length: Int = radius * 2
-    def shift(ofs: Int) = new Centered(center + ofs, radius)
+    def shift(ofs: Int) = Centered(center + ofs, radius)
   }
 }
