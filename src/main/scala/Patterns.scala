c378da134566a7606277556ed656d9aab2ab6ca5
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-12 17:02:54 +0300

    Bind is handled.

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -4,7 +4,7 @@ trait Patterns {
   trait Pattern {
     def get(a: AnyVal): String = a match {
       case 0 => "0"
-      case 1 => "1"
+      case i: Int => i.toString
       case _ => "Unknown"
     }
   }
