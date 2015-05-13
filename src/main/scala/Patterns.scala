b2ff4f3bdbfb79905234460e7a298f6f9277b1d3
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-13 17:29:36 +0300

    Support Pattern Binders

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -3,10 +3,10 @@ package patterns
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = a match {
-      case _: Int => "Int"
-      case 0 | 1 => "0 or 1"
-      case `b` => "b"
-      case scala.math.Pi => "3.14"
+      case c: Int => "Int"
+//      case 0 | 1 => "0 or 1"
+//      case `b` => "b"
+//      case scala.math.Pi => "3.14"
       case _ => "Unknown"
     }
   }
