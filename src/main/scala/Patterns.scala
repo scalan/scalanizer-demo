140f8d8dc2de866e1fa50d7515ed43d1ad01528b
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-13 13:47:28 +0300

    Select pattern is implemented.

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -4,8 +4,8 @@ trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = a match {
       case 0 => "0"
-      case `b` => b.toString
-//      case i: Int => i.toString
+      case `b` => "b"
+      case scala.math.Pi => "3.14"
       case _ => "Unknown"
     }
   }
