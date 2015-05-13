c232b92f2341aacebbc7422deab3bbb72b6a026a
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-13 12:19:44 +0300

    Pattern matching example is extended.

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -2,9 +2,10 @@ package patterns
 
 trait Patterns {
   trait Pattern {
-    def get(a: AnyVal): String = a match {
+    def get(a: Any, b: Int): String = a match {
       case 0 => "0"
-      case i: Int => i.toString
+      case `b` => b.toString
+//      case i: Int => i.toString
       case _ => "Unknown"
     }
   }
