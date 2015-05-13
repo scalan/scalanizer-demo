a99e5e2631c77b35641f1d2c5aef444687446e21
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-13 16:07:44 +0300

    Support Typed Pattern

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -3,6 +3,7 @@ package patterns
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = a match {
+      case _: Int => "Int"
       case 0 | 1 => "0 or 1"
       case `b` => "b"
       case scala.math.Pi => "3.14"
