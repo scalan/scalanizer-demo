e5d3703e46224df69e3d0cb87013d2016a66745d
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-13 15:27:07 +0300

    Alternative pattern is added

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -3,7 +3,7 @@ package patterns
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = a match {
-      case 0 => "0"
+      case 0 | 1 => "0 or 1"
       case `b` => "b"
       case scala.math.Pi => "3.14"
       case _ => "Unknown"
