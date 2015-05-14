846f0e2daf4f7328d8b7103e938d89bedf8700c7
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-14 15:59:16 +0300

    Extractor with one argument

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -2,13 +2,15 @@ package patterns
 
 case class ZeroPat()
 case class OnePat(x: Int)
+case class MultiPat(x: Int, y: Long)
 
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = {
       a match {
-        case ZeroPat() => "Zero"
-        //      case OnePat(y) => y.toString
+        //case MultiPat(0, 1) => "Multi"
+        //case ZeroPat() => "Zero"
+        case OnePat(y) => "one" + y.toString
         //      case c: Int => "Int"
         //      case 0 | 1 => "0 or 1"
         //      case `b` => "b"
