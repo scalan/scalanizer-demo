e43b39b4f394c566823cff76785e9bebab8e0f50
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-14 16:59:23 +0300

    Uncomment all cases

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -2,19 +2,19 @@ package patterns
 
 case class ZeroPat()
 case class OnePat(x: Int)
-case class MultiPat(x: Int, y: Long)
+//case class MultiPat(x: Int, y: Long)
 
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = {
       a match {
         //case MultiPat(0, 1) => "Multi"
-        //case ZeroPat() => "Zero"
+        case ZeroPat() => "Zero"
         case OnePat(y) => "one" + y.toString
-        //      case c: Int => "Int"
-        //      case 0 | 1 => "0 or 1"
-        //      case `b` => "b"
-        //      case scala.math.Pi => "3.14"
+        case c: Int => "Int"
+        case 0 | 1 => "0 or 1"
+        case `b` => "b"
+        case scala.math.Pi => "3.14"
         case _ => "Unknown"
       }
     }
