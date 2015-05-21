e8417047ac701669f5fd10c56fd688ace742adb2
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-21 16:46:13 +0300

    Pattern matching is compiled.

--- src/main/scala/Patterns.scala
+++ src/main/scala/Patterns.scala
@@ -1,21 +1,21 @@
 package patterns
 
-case class ZeroPat()
-case class OnePat(x: Int)
+//case class ZeroPat()
+//case class OnePat[T](x: T)
 //case class MultiPat(x: Int, y: Long)
 
 trait Patterns {
   trait Pattern {
     def get(a: Any, b: Int): String = {
       a match {
-        //case MultiPat(0, 1) => "Multi"
+//        case MultiPat(0, c:Long) => "Multi"
+//        case OnePat(y: OnePat[Int]) => "OnePat"
 //        case ZeroPat() => "Zero"
-//        case OnePat(y) => "one" + y.toString
-//        case 0 | 1 => "0 or 1"
+        case 0 | 1 => "0 or 1"
         case c: Int => c.toString
-//        case `b` => "b"
-//        case scala.math.Pi => "3.14"
-//        case 42 => "42"
+        case `b` => "b"
+        case scala.math.Pi => "3.14"
+        case 42 if b != 42 => "42"
         case _ => "Unknown"
       }
     }
