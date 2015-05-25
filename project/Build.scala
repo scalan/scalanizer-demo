d3c4c717e03107e59497de9f4c3ad911bea2260b
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-25 14:38:43 +0300

    Switching to Scalan CE 0.2.9-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -40,7 +40,7 @@ object ScalanParadiseRootBuild extends Build {
     def addTestConfigsAndCommonSettings =
       p.configs(ItTest).settings(commonSettings: _*)
   }
-  def liteDependency(name: String) = "com.huawei.scalan" %% name % "0.2.8-SNAPSHOT"
+  def liteDependency(name: String) = "com.huawei.scalan" %% name % "0.2.9-SNAPSHOT"
 
   lazy val metaDeps = liteDependency("meta")
   lazy val scalanParadiseMeta = Project(
