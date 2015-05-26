a19447f5d3ebf0eeb15d636e06c7d100d21bf602
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-26 15:53:17 +0300

    Enable the plugin by default

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,8 @@ object ScalanParadiseRootBuild extends Build {
       "-language:higherKinds",
       "-language:implicitConversions",
       "-language:existentials",
-      "-language:postfixOps"
+      "-language:postfixOps",
+      "-Xplugin:/home/mgekk/scalan/scalan-plugin/target/scala-2.11/scalan-plugin_2.11.6-0.0.1-fat.jar"
     ),
     crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.10.5",
                               "2.11.0", "2.11.1", "2.11.2", "2.11.3", "2.11.4", "2.11.5", "2.11.6"
