049d64fb65e58f0a014f6819b413e75cbfca5ae8
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-27 14:51:58 +0300

    Minor changes

--- src/main/scala/paradise/linalgebra/Vectors.scala
+++ src/main/scala/paradise/linalgebra/Vectors.scala
@@ -7,10 +7,8 @@ trait Vectors { self: LinearAlgebra =>
   trait AbstractVector[T] {
     def length: Int
     def items: Collection[T]
-
     def apply(i: Int): T
-    def mapBy[R](f: T => R @uncheckedVariance): Vector[R]
-
+    def map[R](f: T => R): Vector[R]
     def dot(other: Vector[T])(implicit n: Numeric[T]): T
   }
 
@@ -18,8 +16,7 @@ trait Vectors { self: LinearAlgebra =>
     def length = items.length
 
     def apply(i: Int): T = items(i)
-    def mapBy[R](f: T => R @uncheckedVariance): Vector[R] = DenseVector(items.map(f))
-
+    def map[R](f: T => R): Vector[R] = DenseVector(items.map(f))
     def dot(other: Vector[T])(implicit n: Numeric[T]): T = {
       //(other.items zip items).map { case Pair(v1, v2) => v1 * v2 }.reduce
       ???
