906b86ba0227a8a3fd7cec0d43993492775da623
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-27 15:11:04 +0300

    The * method is implemented.

--- src/main/scala/paradise/linalgebra/Matrices.scala
+++ src/main/scala/paradise/linalgebra/Matrices.scala
@@ -5,25 +5,24 @@ trait Matrices { self: LinearAlgebra =>
   trait AbstractMatrix[T] {
     def numColumns: Int
     def numRows: Int
-    def rows: Collection[AbstractVector[T]]
-    def columns(implicit n: Numeric[T]): Collection[AbstractVector[T]]
+    def rows: Collection[Vector[T]]
+    def columns(implicit n: Numeric[T]): Collection[Vector[T]]
     def *(vector: Vector[T])(implicit n: Numeric[T]): Vector[T] = {
-      DenseVector(rows.map { r: AbstractVector[T] => r.dot(vector) })
+      DenseVector(rows.map { r: Vector[T] => r.dot(vector) })
     }
   }
 
-  case class CompoundMatrix[T](val rows: Collection[AbstractVector[T]], val numColumns: Int)
+  case class CompoundMatrix[T](val rows: Collection[Vector[T]], val numColumns: Int)
     extends AbstractMatrix[T] {
 
     def numRows = rows.length
-    def columns(implicit n: Numeric[T]): Collection[AbstractVector[T]] = {
+    def columns(implicit n: Numeric[T]): Collection[Vector[T]] = {
       ??? //Collection(SArray.tabulate(numColumns) { j => DenseVector(rows.map(_(j)))})
     }
 
     def *(matrix: Matrix[T])(implicit n: Numeric[T]): Matrix[T] = {
-//      val mT = matrix.companion.fromRows(matrix.columns, matrix.numRows)
-//      companion(self.rows.map(row => mT * row), matrix.numColumns)
-      ???
+      val mT = CompoundMatrix(matrix.columns, matrix.numRows)
+      CompoundMatrix(this.rows.map((row: Vector[T]) => mT * row), matrix.numColumns)
     }
   }
 }
