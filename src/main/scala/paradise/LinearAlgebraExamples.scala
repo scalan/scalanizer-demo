8e3f9974bbcf51683e5c159c192157019df81543
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-27 15:40:00 +0300

    The structure of LinearAlgebraExamples is changed

--- src/main/scala/paradise/LinearAlgebraExamples.scala
+++ src/main/scala/paradise/LinearAlgebraExamples.scala
@@ -4,11 +4,15 @@ import linalgebra._
 import paradise.collections.Collections
 
 trait LinearAlgebraExamples extends LinearAlgebra with Collections {
-  def ddmvm(m: Array[Array[Double]], v: Array[Double]) = {
-    val width = m(0).length
-    val matrix: Matrix[Double] = CompoundMatrix(Collection(m.map { r: Array[Double] => DenseVector(Collection(r)) }), width)
-    val vector: Vector[Double] = DenseVector(Collection(v))
 
-    (matrix * vector).items.arr
+  trait LinearAlgebraExample {
+    def ddmvm(m: Array[Array[Double]], v: Array[Double]) = {
+      val width = m(0).length
+      val matrix: Matrix[Double] = CompoundMatrix(Collection(m.map { r: Array[Double] => DenseVector(Collection(r)) }), width)
+      val vector: Vector[Double] = DenseVector(Collection(v))
+
+      (matrix * vector).items.arr
+    }
   }
+
 }
--- src/test/scala/paradise/LinearAlgebraTests.scala
+++ src/test/scala/paradise/LinearAlgebraTests.scala
@@ -6,7 +6,8 @@ class LinearAlgebraTests extends FunSuite with LinearAlgebraExamples {
   test("ddmvm") {
     val inM = Array(Array(1.0, 1.0), Array(0.0, 1.0))
     val inV = Array(2.0, 3.0)
+    val laExample = new LinearAlgebraExample {}
 
-    assertResult(Array(5.0, 3.0))(ddmvm(inM, inV))
+    assertResult(Array(5.0, 3.0))(laExample.ddmvm(inM, inV))
   }
 }
