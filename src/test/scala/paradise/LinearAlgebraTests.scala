be80b22f3fff5568d458a232527ad4eae9ab9481
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-27 14:37:25 +0300

    A test is added.

---
+++ src/test/scala/paradise/LinearAlgebraTests.scala
@@ -0,0 +1,12 @@
+package paradise
+
+import org.scalatest._
+
+class LinearAlgebraTests extends FunSuite with LinearAlgebraExamples {
+  test("ddmvm") {
+    val inM = Array(Array(1.0, 1.0), Array(0.0, 1.0))
+    val inV = Array(2.0, 3.0)
+
+    assertResult(Array(5.0, 3.0))(ddmvm(inM, inV))
+  }
+}
