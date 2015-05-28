b51b76abe2365e81f9176aa93755694c1ba5fdb3
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-28 17:16:43 +0300

    LinearAlgebraDsl is added manually

--- src/main/scala/paradise/linalgebra/LinearAlgebra.scala
+++ src/main/scala/paradise/linalgebra/LinearAlgebra.scala
@@ -6,3 +6,13 @@ import paradise.collections._
 trait LinearAlgebra extends Collections with Monoids
 with Vectors
 with Matrices
+
+import scalan._
+trait LinearAlgebraDsl extends Scalan
+with LinearAlgebra
+
+trait LinearAlgebraDslSeq extends ScalanSeq
+with LinearAlgebraDsl
+
+trait LinearAlgebraDslExp extends ScalanExp
+with LinearAlgebraDsl
