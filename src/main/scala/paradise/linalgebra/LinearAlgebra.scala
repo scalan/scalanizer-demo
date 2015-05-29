53d73006ffc16d7bf43819cb819dedf304a0ba78
Maxim Gekk <gekk.maxim@huawei.com>
2015-05-29 15:44:22 +0300

    Boilerplate for linear algebras

--- src/main/scala/paradise/linalgebra/LinearAlgebra.scala
+++ src/main/scala/paradise/linalgebra/LinearAlgebra.scala
@@ -3,16 +3,32 @@ package paradise.linalgebra
 import paradise._
 import paradise.collections._
 
-trait LinearAlgebra extends Collections with Monoids
+trait LinearAlgebra extends Monoids
+with Collections
 with Vectors
 with Matrices
-
+/*
 import scalan._
+import paradise.collections.implOfCollections.StagedEvaluation._
+import paradise.implOfMonoids.StagedEvaluation._
+import paradise.linalgebra.implOfVectors.StagedEvaluation._
+import paradise.linalgebra.implOfMatrices.StagedEvaluation._
+
 trait LinearAlgebraDsl extends Scalan
-with LinearAlgebra
+with MonoidsDsl
+with CollectionsDsl
+with VectorsDsl
+with MatricesDsl
 
-trait LinearAlgebraDslSeq extends ScalanSeq
-with LinearAlgebraDsl
+trait LinearAlgebraDslSeq extends ScalanSeq with LinearAlgebraDsl
+with MonoidsDslSeq
+with CollectionsDslSeq
+with VectorsDslSeq
+with MatricesDslSeq
 
-trait LinearAlgebraDslExp extends ScalanExp
-with LinearAlgebraDsl
+trait LinearAlgebraDslExp extends ScalanExp with LinearAlgebraDsl
+with MonoidsDslExp
+with CollectionsDslExp
+with VectorsDslExp
+with MatricesDslExp
+*/
