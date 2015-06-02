6099bd893d74e366e13aa4b34c850e9990f534cd
Maxim Gekk <gekk.maxim@huawei.com>
2015-06-02 10:16:43 +0300

    NumMonoids is compiled.

--- src/main/scala/paradise/impl/NumMonoidsImpl.scala
+++ src/main/scala/paradise/impl/NumMonoidsImpl.scala
@@ -219,7 +219,7 @@ package paradise {
           case _ => None
         };
         object NumMonoidMethods {
-          object opName {
+          object n {
             def unapply(d: (Def[_$15] forSome { 
               type _$15
             })): Option[(Rep[NumMonoid[A]] forSome { 
@@ -228,7 +228,7 @@ package paradise {
               case MethodCall((receiver @ _), (method @ _), _, _) if receiver.elem.isInstanceOf[(NumMonoidElem[_$16, _$17] forSome { 
   type _$16;
   type _$17
-})].&&(method.getName.==("opName")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
+})].&&(method.getName.==("n")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
                 type A
               })]]
               case _ => None
@@ -242,7 +242,7 @@ package paradise {
               case _ => None
             }
           };
-          object zero {
+          object opName {
             def unapply(d: (Def[_$19] forSome { 
               type _$19
             })): Option[(Rep[NumMonoid[A]] forSome { 
@@ -251,7 +251,7 @@ package paradise {
               case MethodCall((receiver @ _), (method @ _), _, _) if receiver.elem.isInstanceOf[(NumMonoidElem[_$20, _$21] forSome { 
   type _$20;
   type _$21
-})].&&(method.getName.==("zero")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
+})].&&(method.getName.==("opName")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
                 type A
               })]]
               case _ => None
@@ -265,7 +265,7 @@ package paradise {
               case _ => None
             }
           };
-          object append {
+          object zero {
             def unapply(d: (Def[_$23] forSome { 
               type _$23
             })): Option[(Rep[NumMonoid[A]] forSome { 
@@ -274,7 +274,7 @@ package paradise {
               case MethodCall((receiver @ _), (method @ _), _, _) if receiver.elem.isInstanceOf[(NumMonoidElem[_$24, _$25] forSome { 
   type _$24;
   type _$25
-})].&&(method.getName.==("append")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
+})].&&(method.getName.==("zero")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
                 type A
               })]]
               case _ => None
@@ -288,7 +288,7 @@ package paradise {
               case _ => None
             }
           };
-          object isCommutative {
+          object append {
             def unapply(d: (Def[_$27] forSome { 
               type _$27
             })): Option[(Rep[NumMonoid[A]] forSome { 
@@ -297,7 +297,7 @@ package paradise {
               case MethodCall((receiver @ _), (method @ _), _, _) if receiver.elem.isInstanceOf[(NumMonoidElem[_$28, _$29] forSome { 
   type _$28;
   type _$29
-})].&&(method.getName.==("isCommutative")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
+})].&&(method.getName.==("append")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
                 type A
               })]]
               case _ => None
@@ -310,14 +310,37 @@ package paradise {
               case Def((d @ _)) => unapply(d)
               case _ => None
             }
+          };
+          object isCommutative {
+            def unapply(d: (Def[_$31] forSome { 
+              type _$31
+            })): Option[(Rep[NumMonoid[A]] forSome { 
+              type A
+            })] = d match {
+              case MethodCall((receiver @ _), (method @ _), _, _) if receiver.elem.isInstanceOf[(NumMonoidElem[_$32, _$33] forSome { 
+  type _$32;
+  type _$33
+})].&&(method.getName.==("isCommutative")) => Some(receiver).asInstanceOf[Option[(Rep[NumMonoid[A]] forSome { 
+                type A
+              })]]
+              case _ => None
+            };
+            def unapply(exp: (Exp[_$34] forSome { 
+              type _$34
+            })): Option[(Rep[NumMonoid[A]] forSome { 
+              type A
+            })] = exp match {
+              case Def((d @ _)) => unapply(d)
+              case _ => None
+            }
           }
         };
         object NumMonoidCompanionMethods
       };
       trait NumMonoids extends Base with NumersDsl { self: NumMonoidsDsl =>
         trait NumMonoid[A] extends Reifiable[NumMonoid[A]] {
-          implicit def n: Numer[A]
           implicit def eA: Elem[A];
+          implicit def n: Numer[A];
           def opName: Rep[String];
           def zero: Rep[A];
           def append: Rep[scala.Function1[scala.Tuple2[A, A], A]];
@@ -345,7 +368,7 @@ package paradise {
       trait NumMonoidsDslExp extends NumMonoidsExp with NumersDslExp { self: NumMonoidsDslExp =>
         
       };
-      val serializedMetaAst = "rO0ABXNyACZzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU0VudGl0eU1vZHVsZURlZqplOx8xRQC7AgAMTAAJYW5jZXN0b3JzdAAhTHNjYWxhL2NvbGxlY3Rpb24vaW1tdXRhYmxlL0xpc3Q7TAAEYm9keXEAfgABTAAQY29uY3JldGVTQ2xhc3Nlc3EAfgABTAAIZW50aXRpZXNxAH4AAUwACWVudGl0eU9wc3QAIUxzY2FsYW4vbWV0YS9TY2FsYW5Bc3QkU1RyYWl0RGVmO0wAEGVudGl0eVJlcFN5bm9ueW10AA5Mc2NhbGEvT3B0aW9uO0wAB2ltcG9ydHNxAH4AAUwAB21ldGhvZHNxAH4AAUwABG5hbWV0ABJMamF2YS9sYW5nL1N0cmluZztMAAtwYWNrYWdlTmFtZXEAfgAETAAIc2VsZlR5cGVxAH4AA0wACnNlcURzbEltcGxxAH4AA3hwc3IAMnNjYWxhLmNvbGxlY3Rpb24uaW1tdXRhYmxlLkxpc3QkU2VyaWFsaXphdGlvblByb3h5AAAAAAAAAAEDAAB4cHNyACBzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU1RyYWl0Q2FsbFDrGS0l7EVYAgACTAAEbmFtZXEAfgAETAAJdHBlU0V4cHJzcQB+AAF4cHQABk51bWVyc3NxAH4ABnNyACxzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS5MaXN0U2VyaWFsaXplRW5kJIpcY1v3UwttAgAAeHB4cQB+AA14cQB+AAtzcQB+AAZzcgAfc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNDbGFzc0RlZgfQfWjxqHAzAgAKWgAKaXNBYnN0cmFjdEwACWFuY2VzdG9yc3EAfgABTAALYW5ub3RhdGlvbnNxAH4AAUwABGFyZ3N0ACJMc2NhbGFuL21ldGEvU2NhbGFuQXN0JFNDbGFzc0FyZ3M7TAAEYm9keXEAfgABTAAJY29tcGFuaW9ucQB+AANMAAxpbXBsaWNpdEFyZ3NxAH4AEEwABG5hbWVxAH4ABEwACHNlbGZUeXBlcQB+AANMAAd0cGVBcmdzcQB+AAF4cABzcQB+AAZzcQB+AAh0AAlOdW1Nb25vaWRzcQB+AAZzcQB+AAh0AAFBcQB+AAtxAH4ADXhzcQB+AAh0AAdQcm9kdWN0cQB+AAtzcQB+AAh0AAxTZXJpYWxpemFibGVxAH4AC3EAfgANeHEAfgALc3IAIHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ2xhc3NBcmdzJ+vuZMUwAFwCAAFMAARhcmdzcQB+AAF4cHEAfgALc3EAfgAGc3IAIHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTTWV0aG9kRGVm3VlxcbcwKw4CAApaAAxpc0VsZW1PckNvbnRaAAppc0ltcGxpY2l0WgAKaXNPdmVycmlkZUwAC2Fubm90YXRpb25zcQB+AAFMAAthcmdTZWN0aW9uc3EAfgABTAAEYm9keXEAfgADTAAEbmFtZXEAfgAETAAKb3ZlcmxvYWRJZHEAfgADTAAHdHBlQXJnc3EAfgABTAAGdHBlUmVzcQB+AAN4cAAAAHEAfgALcQB+AAtzcgAKc2NhbGEuU29tZREi8mleoYt0AgABTAABeHQAEkxqYXZhL2xhbmcvT2JqZWN0O3hyAAxzY2FsYS5PcHRpb27+aTf92w5mdAIAAHhwc3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ29uc3QaTRTRYktnigIAAUwAAWNxAH4AInhwdAABK3QABm9wTmFtZXNyAAtzY2FsYS5Ob25lJEZQJPZTypSsAgAAeHEAfgAjcQB+AAtxAH4AKnNxAH4AHwAAAHEAfgALcQB+AAtzcQB+ACFzcgAdc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNTZWxlY3QL3WllZUD5hQIAAkwABGV4cHJ0AB1Mc2NhbGFuL21ldGEvU2NhbGFuQXN0JFNFeHByO0wABXRuYW1lcQB+AAR4cHNyABxzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU0lkZW50JxKl+/AJ108CAAFMAARuYW1lcQB+AAR4cHQAAW50AAR6ZXJvcQB+ADNxAH4AKnEAfgALcQB+ACpzcQB+AB8AAABxAH4AC3EAfgALc3EAfgAhc3IAG3NjYWxhbi5tZXRhLlNjYWxhbkFzdCRTRnVuY7BM+JAaxTCDAgACTAAGcGFyYW1zcQB+AAFMAANyZXNxAH4ALnhwc3EAfgAGc3IAHXNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVmFsRGVmKwY5unvdRQ8CAAVaAAppc0ltcGxpY2l0WgAGaXNMYXp5TAAEZXhwcnEAfgAuTAAEbmFtZXEAfgAETAADdHBlcQB+AAN4cAAAc3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTRW1wdHmRYBXhS+VovgIAAHhwdAACYTBzcQB+ACFzcQB+AAh0AAFBcQB+AAtzcQB+ADkAAHNxAH4AO3QAAmExc3EAfgAhc3EAfgAIdAABQXEAfgALcQB+AA14c3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQXBwbHmEZimf0pr8fwIAA0wABWFyZ3NzcQB+AAFMAANmdW5xAH4ALkwAAnRzcQB+AAF4cHNxAH4ABnNxAH4ABnNxAH4AMHQAAmEwc3EAfgAwdAACYTFxAH4ADXhxAH4ADXhzcQB+AC1zcQB+ADB0AAFudAAEcGx1c3EAfgALdAAGYXBwZW5kcQB+ACpxAH4AC3EAfgAqc3EAfgAfAAAAcQB+AAtxAH4AC3NxAH4AIXNxAH4AJXNyABFqYXZhLmxhbmcuQm9vbGVhbs0gcoDVnPruAgABWgAFdmFsdWV4cAF0AA1pc0NvbW11dGF0aXZlcQB+ACpxAH4AC3NxAH4AIXNyACNzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU1RwZVByaW1pdGl2ZTXDu1Xwvk4FAgACTAASZGVmYXVsdFZhbHVlU3RyaW5ncQB+AARMAARuYW1lcQB+AAR4cHQABWZhbHNldAAHQm9vbGVhbnEAfgANeHEAfgAqc3EAfgAcc3EAfgAGc3IAH3NjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ2xhc3NBcmcFc9jWC9QpPwIACFoAB2ltcEZsYWdaAAxpc0VsZW1PckNvbnRaAAhvdmVyRmxhZ1oAB3ZhbEZsYWdMAAthbm5vdGF0aW9uc3EAfgABTAAHZGVmYXVsdHEAfgADTAAEbmFtZXEAfgAETAADdHBldAAgTHNjYWxhbi9tZXRhL1NjYWxhbkFzdCRTVHBlRXhwcjt4cAEAAAFxAH4AC3EAfgAqdAABbnNxAH4ACHQABU51bWVyc3EAfgAGc3EAfgAIdAABQXEAfgALcQB+AA14cQB+AA14dAAKUGx1c01vbm9pZHEAfgAqc3EAfgAGc3IAHXNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVHBlQXJnklsmSHLxm4kCAARMAAVib3VuZHEAfgADTAAMY29udGV4dEJvdW5kcQB+AAFMAARuYW1lcQB+AARMAAd0cGFyYW1zcQB+AAF4cHEAfgAqcQB+AAt0AAFBcQB+AAtxAH4ADXhxAH4ADXhzcQB+AAZzcgAfc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNUcmFpdERlZgG+NJI0FLGyAgAJWgAIYml0bWFwJDBMAAlhbmNlc3RvcnNxAH4AAUwAC2Fubm90YXRpb25zcQB+AAFMAARib2R5cQB+AAFMAAljb21wYW5pb25xAH4AA0wADGltcGxpY2l0QXJnc3EAfgAQTAAEbmFtZXEAfgAETAAIc2VsZlR5cGVxAH4AA0wAB3RwZUFyZ3NxAH4AAXhwAHEAfgALcQB+AAtzcQB+AAZzcQB+AB8AAABxAH4AC3EAfgALcQB+ACp0AAZvcE5hbWVxAH4AKnEAfgALc3EAfgAhc3EAfgBbdAACIiJ0AAZTdHJpbmdzcQB+AB8AAABxAH4AC3EAfgALcQB+ACpxAH4AM3EAfgAqcQB+AAtzcQB+ACFzcQB+AAh0AAFBcQB+AAtzcQB+AB8AAABxAH4AC3EAfgALcQB+ACp0AAZhcHBlbmRxAH4AKnEAfgALc3EAfgAhc3IAHnNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVHBlRnVuY9RJmRMPHV/hAgACTAAGZG9tYWlucQB+AGJMAAVyYW5nZXEAfgBieHBzcgAfc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNUcGVUdXBsZTS7+V6IgEhwAgABTAAJdHBlU0V4cHJzcQB+AAF4cHNxAH4ABnNxAH4ACHQAAUFxAH4AC3NxAH4ACHQAAUFxAH4AC3EAfgANeHNxAH4ACHQAAUFxAH4AC3NxAH4AHwAAAHEAfgALcQB+AAtxAH4AKnQADWlzQ29tbXV0YXRpdmVxAH4AKnEAfgALc3EAfgAhcQB+AFxxAH4ADXhxAH4AKnB0AAlOdW1Nb25vaWRxAH4AKnNxAH4ABnNxAH4AbHEAfgAqcQB+AAt0AAFBcQB+AAtxAH4ADXhxAH4ADXhxAH4AcXEAfgAqcQB+AAtxAH4AC3QACk51bU1vbm9pZHN0AAhwYXJhZGlzZXEAfgAqcQB+ACo="
+      val serializedMetaAst = "rO0ABXNyACZzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU0VudGl0eU1vZHVsZURlZqplOx8xRQC7AgAMTAAJYW5jZXN0b3JzdAAhTHNjYWxhL2NvbGxlY3Rpb24vaW1tdXRhYmxlL0xpc3Q7TAAEYm9keXEAfgABTAAQY29uY3JldGVTQ2xhc3Nlc3EAfgABTAAIZW50aXRpZXNxAH4AAUwACWVudGl0eU9wc3QAIUxzY2FsYW4vbWV0YS9TY2FsYW5Bc3QkU1RyYWl0RGVmO0wAEGVudGl0eVJlcFN5bm9ueW10AA5Mc2NhbGEvT3B0aW9uO0wAB2ltcG9ydHNxAH4AAUwAB21ldGhvZHNxAH4AAUwABG5hbWV0ABJMamF2YS9sYW5nL1N0cmluZztMAAtwYWNrYWdlTmFtZXEAfgAETAAIc2VsZlR5cGVxAH4AA0wACnNlcURzbEltcGxxAH4AA3hwc3IAMnNjYWxhLmNvbGxlY3Rpb24uaW1tdXRhYmxlLkxpc3QkU2VyaWFsaXphdGlvblByb3h5AAAAAAAAAAEDAAB4cHNyACBzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU1RyYWl0Q2FsbFDrGS0l7EVYAgACTAAEbmFtZXEAfgAETAAJdHBlU0V4cHJzcQB+AAF4cHQABk51bWVyc3NxAH4ABnNyACxzY2FsYS5jb2xsZWN0aW9uLmltbXV0YWJsZS5MaXN0U2VyaWFsaXplRW5kJIpcY1v3UwttAgAAeHB4cQB+AA14cQB+AAtzcQB+AAZzcgAfc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNDbGFzc0RlZgfQfWjxqHAzAgAKWgAKaXNBYnN0cmFjdEwACWFuY2VzdG9yc3EAfgABTAALYW5ub3RhdGlvbnNxAH4AAUwABGFyZ3N0ACJMc2NhbGFuL21ldGEvU2NhbGFuQXN0JFNDbGFzc0FyZ3M7TAAEYm9keXEAfgABTAAJY29tcGFuaW9ucQB+AANMAAxpbXBsaWNpdEFyZ3NxAH4AEEwABG5hbWVxAH4ABEwACHNlbGZUeXBlcQB+AANMAAd0cGVBcmdzcQB+AAF4cABzcQB+AAZzcQB+AAh0AAlOdW1Nb25vaWRzcQB+AAZzcQB+AAh0AAFBcQB+AAtxAH4ADXhzcQB+AAh0AAdQcm9kdWN0cQB+AAtzcQB+AAh0AAxTZXJpYWxpemFibGVxAH4AC3EAfgANeHEAfgALc3IAIHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ2xhc3NBcmdzJ+vuZMUwAFwCAAFMAARhcmdzcQB+AAF4cHEAfgALc3EAfgAGc3IAIHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTTWV0aG9kRGVm3VlxcbcwKw4CAApaAAxpc0VsZW1PckNvbnRaAAppc0ltcGxpY2l0WgAKaXNPdmVycmlkZUwAC2Fubm90YXRpb25zcQB+AAFMAAthcmdTZWN0aW9uc3EAfgABTAAEYm9keXEAfgADTAAEbmFtZXEAfgAETAAKb3ZlcmxvYWRJZHEAfgADTAAHdHBlQXJnc3EAfgABTAAGdHBlUmVzcQB+AAN4cAAAAHEAfgALcQB+AAtzcgAKc2NhbGEuU29tZREi8mleoYt0AgABTAABeHQAEkxqYXZhL2xhbmcvT2JqZWN0O3hyAAxzY2FsYS5PcHRpb27+aTf92w5mdAIAAHhwc3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ29uc3QaTRTRYktnigIAAUwAAWNxAH4AInhwdAABK3QABm9wTmFtZXNyAAtzY2FsYS5Ob25lJEZQJPZTypSsAgAAeHEAfgAjcQB+AAtxAH4AKnNxAH4AHwAAAHEAfgALcQB+AAtzcQB+ACFzcgAdc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNTZWxlY3QL3WllZUD5hQIAAkwABGV4cHJ0AB1Mc2NhbGFuL21ldGEvU2NhbGFuQXN0JFNFeHByO0wABXRuYW1lcQB+AAR4cHNyABxzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU0lkZW50JxKl+/AJ108CAAFMAARuYW1lcQB+AAR4cHQAAW50AAR6ZXJvcQB+ADNxAH4AKnEAfgALcQB+ACpzcQB+AB8AAABxAH4AC3EAfgALc3EAfgAhc3IAG3NjYWxhbi5tZXRhLlNjYWxhbkFzdCRTRnVuY7BM+JAaxTCDAgACTAAGcGFyYW1zcQB+AAFMAANyZXNxAH4ALnhwc3EAfgAGc3IAHXNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVmFsRGVmKwY5unvdRQ8CAAVaAAppc0ltcGxpY2l0WgAGaXNMYXp5TAAEZXhwcnEAfgAuTAAEbmFtZXEAfgAETAADdHBlcQB+AAN4cAAAc3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTRW1wdHmRYBXhS+VovgIAAHhwdAACYTBzcQB+ACFzcQB+AAh0AAFBcQB+AAtzcQB+ADkAAHNxAH4AO3QAAmExc3EAfgAhc3EAfgAIdAABQXEAfgALcQB+AA14c3IAHHNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQXBwbHmEZimf0pr8fwIAA0wABWFyZ3NzcQB+AAFMAANmdW5xAH4ALkwAAnRzcQB+AAF4cHNxAH4ABnNxAH4ABnNxAH4AMHQAAmEwc3EAfgAwdAACYTFxAH4ADXhxAH4ADXhzcQB+AC1zcQB+ADB0AAFudAAEcGx1c3EAfgALdAAGYXBwZW5kcQB+ACpxAH4AC3EAfgAqc3EAfgAfAAAAcQB+AAtxAH4AC3NxAH4AIXNxAH4AJXNyABFqYXZhLmxhbmcuQm9vbGVhbs0gcoDVnPruAgABWgAFdmFsdWV4cAF0AA1pc0NvbW11dGF0aXZlcQB+ACpxAH4AC3NxAH4AIXNyACNzY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU1RwZVByaW1pdGl2ZTXDu1Xwvk4FAgACTAASZGVmYXVsdFZhbHVlU3RyaW5ncQB+AARMAARuYW1lcQB+AAR4cHQABWZhbHNldAAHQm9vbGVhbnEAfgANeHEAfgAqc3EAfgAcc3EAfgAGc3IAH3NjYWxhbi5tZXRhLlNjYWxhbkFzdCRTQ2xhc3NBcmcFc9jWC9QpPwIACFoAB2ltcEZsYWdaAAxpc0VsZW1PckNvbnRaAAhvdmVyRmxhZ1oAB3ZhbEZsYWdMAAthbm5vdGF0aW9uc3EAfgABTAAHZGVmYXVsdHEAfgADTAAEbmFtZXEAfgAETAADdHBldAAgTHNjYWxhbi9tZXRhL1NjYWxhbkFzdCRTVHBlRXhwcjt4cAEAAAFxAH4AC3EAfgAqdAABbnNxAH4ACHQABU51bWVyc3EAfgAGc3EAfgAIdAABQXEAfgALcQB+AA14cQB+AA14dAAKUGx1c01vbm9pZHEAfgAqc3EAfgAGc3IAHXNjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVHBlQXJnklsmSHLxm4kCAARMAAVib3VuZHEAfgADTAAMY29udGV4dEJvdW5kcQB+AAFMAARuYW1lcQB+AARMAAd0cGFyYW1zcQB+AAF4cHEAfgAqcQB+AAt0AAFBcQB+AAtxAH4ADXhxAH4ADXhzcQB+AAZzcgAfc2NhbGFuLm1ldGEuU2NhbGFuQXN0JFNUcmFpdERlZgG+NJI0FLGyAgAJWgAIYml0bWFwJDBMAAlhbmNlc3RvcnNxAH4AAUwAC2Fubm90YXRpb25zcQB+AAFMAARib2R5cQB+AAFMAAljb21wYW5pb25xAH4AA0wADGltcGxpY2l0QXJnc3EAfgAQTAAEbmFtZXEAfgAETAAIc2VsZlR5cGVxAH4AA0wAB3RwZUFyZ3NxAH4AAXhwAHEAfgALcQB+AAtzcQB+AAZzcQB+AB8AAQBxAH4AC3EAfgALcQB+ACp0AAFucQB+ACpxAH4AC3NxAH4AIXNxAH4ACHQABU51bWVyc3EAfgAGc3EAfgAIdAABQXEAfgALcQB+AA14c3EAfgAfAAAAcQB+AAtxAH4AC3EAfgAqdAAGb3BOYW1lcQB+ACpxAH4AC3NxAH4AIXNxAH4AW3QAAiIidAAGU3RyaW5nc3EAfgAfAAAAcQB+AAtxAH4AC3EAfgAqcQB+ADNxAH4AKnEAfgALc3EAfgAhc3EAfgAIdAABQXEAfgALc3EAfgAfAAAAcQB+AAtxAH4AC3EAfgAqdAAGYXBwZW5kcQB+ACpxAH4AC3NxAH4AIXNyAB5zY2FsYW4ubWV0YS5TY2FsYW5Bc3QkU1RwZUZ1bmPUSZkTDx1f4QIAAkwABmRvbWFpbnEAfgBiTAAFcmFuZ2VxAH4AYnhwc3IAH3NjYWxhbi5tZXRhLlNjYWxhbkFzdCRTVHBlVHVwbGU0u/leiIBIcAIAAUwACXRwZVNFeHByc3EAfgABeHBzcQB+AAZzcQB+AAh0AAFBcQB+AAtzcQB+AAh0AAFBcQB+AAtxAH4ADXhzcQB+AAh0AAFBcQB+AAtzcQB+AB8AAABxAH4AC3EAfgALcQB+ACp0AA1pc0NvbW11dGF0aXZlcQB+ACpxAH4AC3NxAH4AIXEAfgBccQB+AA14cQB+ACpwdAAJTnVtTW9ub2lkcQB+ACpzcQB+AAZzcQB+AGxxAH4AKnEAfgALdAABQXEAfgALcQB+AA14cQB+AA14cQB+AHFxAH4AKnEAfgALcQB+AAt0AApOdW1Nb25vaWRzdAAIcGFyYWRpc2VxAH4AKnEAfgAq"
     }
   }
 }
