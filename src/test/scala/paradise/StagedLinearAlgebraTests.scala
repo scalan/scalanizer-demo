fcf9dab7d0d27b5aa6b91cf28ab1ea09b8db90b8
Maxim Gekk <gekk.maxim@huawei.com>
2015-06-02 11:21:38 +0300

    The example is modified to be metched to tests from scalan-spark

--- src/test/scala/paradise/StagedLinearAlgebraTests.scala
+++ src/test/scala/paradise/StagedLinearAlgebraTests.scala
@@ -1,15 +1,18 @@
 package paradise
 
 import scalan._
+import scala.language.reflectiveCalls
 import paradise.implOfLinearAlgebraExamples.StagedEvaluation._
 
-trait StagedLinearAlgebraTests extends BaseTests {
-  class LaStaged(testName: String) extends TestContext(this, testName) with LinearAlgebraExamplesDslExp {
+trait StagedLinearAlgebraTests extends BaseTests { suit =>
+
+  trait SimpleLaTests extends ScalanDsl with LinearAlgebraExamplesDsl {
+    lazy val t0 = fun {(in: Rep[MvMExample]) => in.ddmvm0}
   }
 
   test("simple") {
-    val ctx = new LaStaged("mvm")
+    val ctx = new TestContext(this, "simpleStagedLa") with SimpleLaTests with LinearAlgebraExamplesDslExp
 
-    //ctx.emit("ddmvm", ctx)
+    ctx.emit("t0", ctx.t0)
   }
 }
