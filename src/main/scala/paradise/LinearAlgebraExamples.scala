2fd914ebfd467a477984e01db6fafcf696936f40
Zotov Yuriy <zotov.yuriy@huawei.com>
2015-06-03 18:25:04 +0300

    print messages in ddmvm test

--- src/main/scala/paradise/LinearAlgebraExamples.scala
+++ src/main/scala/paradise/LinearAlgebraExamples.scala
@@ -3,10 +3,23 @@ package paradise
 import paradise.linalgebra._
 
 import scala.reflect.ClassTag
+import scala.util.Random
 
 trait LinearAlgebraExamples extends LinearAlgebra {
 
   trait LinearAlgebraExample
+
+  object CommonData {
+    val rnd = new Random(1)
+
+    def genArray(len: Int): Array[Double] = {
+      { for (i <- 1 to len) yield { rnd.nextDouble() } }.toArray
+    }
+
+    def genMatr(rows: Int, cols: Int): Array[Array[Double]] = {
+      { for (i <- 1 to rows) yield { genArray(cols) } }.toArray
+    }
+  }
 
   case class MvMExample() extends LinearAlgebraExample {
     implicit val doubleNumer: Numer[Double] = DoubleNumer()
@@ -17,11 +30,13 @@ trait LinearAlgebraExamples extends LinearAlgebra {
       DenseVector(matrix.rows.map{ r: AbstractVector[T] => r dot vector })
     }
 
-    lazy val ddmvm0 = (m: Array[Array[Double]], v: Array[Double]) => {
+    def ddmvm(m: Array[Array[Double]], v: Array[Double]): Array[Double] = {
       val width = m(0).length
       val matrix: AbstractMatrix[Double] = CompoundMatrix(Collection(m.map { r: Array[Double] => DenseVector(Collection(r)) }), width)
       val vector: AbstractVector[Double] = DenseVector(Collection(v))
       mvm(matrix, vector).items.arr
     }
+
+    lazy val ddmvm0 = ddmvm(_,_)
   }
 }
--- src/test/scala/paradise/LinearAlgebraTests.scala
+++ src/test/scala/paradise/LinearAlgebraTests.scala
@@ -3,10 +3,29 @@ package paradise
 import org.scalatest._
 
 class LinearAlgebraTests extends FunSuite with LinearAlgebraExamples {
-  test("ddmvm") {
-    val inM = Array(Array(1.0, 1.0), Array(0.0, 1.0))
-    val inV = Array(2.0, 3.0)
 
-    assertResult(Array(5.0, 3.0))(MvMExample().ddmvm0(inM, inV))
+
+
+  test("ddmvm") {
+    val rows = 10000
+    val cols = rows;
+
+    println(s"""Generating random ${rows}x${cols} dense "Array[Array[Double]]" matrix...""")
+    val inM = CommonData.genMatr(rows, cols)
+    println(s"""Generating random ${rows} elements "Array[Double]" vector...""")
+    val inV = CommonData.genArray(cols)
+
+    val correctRes = inM.map({r:Array[Double] => r.zip(inV).map({p => p._1 * p._2}).fold(0.0)(_+_)})
+
+    println( s"Multiplying matrix by vector..." )
+    val t1 = System.currentTimeMillis()
+    val res = MvMExample().ddmvm0(inM, inV)
+    val t2 = System.currentTimeMillis()
+
+    assertResult(correctRes)(res)
+
+    val dt = t2 - t1
+    println("")
+    println(s"Multiplication is done in $dt millis.")
   }
 }
