7a4cf2e07eb7e6467b13235c773adb6ba3d910db
Maxim Gekk <gekk.maxim@huawei.com>
2015-06-03 14:21:39 +0300

    implicits were moved into method's body

--- src/main/scala/paradise/LinearAlgebraExamples.scala
+++ src/main/scala/paradise/LinearAlgebraExamples.scala
@@ -3,13 +3,15 @@ package paradise
 import paradise.linalgebra._
 
 trait LinearAlgebraExamples extends LinearAlgebra {
-  implicit val doubleNumer: Numer[Double] = DoubleNumer()
-  implicit val plusMonoid: NumMonoid[Double] = PlusMonoid[Double]
-
   @HotSpot
   def ddmvm0(m: Array[Array[Double]], v: Array[Double]) = {
+    implicit val doubleNumer: Numer[Double] = DoubleNumer()
+    implicit val plusMonoid: NumMonoid[Double] = PlusMonoid[Double]
+
     val width = m(0).length
-    val matrix: AbstractMatrix[Double] = CompoundMatrix(Collection(m.map { r: Array[Double] => DenseVector(Collection(r)) }), width)
+    val matrix: AbstractMatrix[Double] = {
+      CompoundMatrix(Collection(m.map { r: Array[Double] => DenseVector(Collection(r)) }), width)
+    }
     val vector: AbstractVector[Double] = DenseVector(Collection(v))
 
     BaseOp().mvm(matrix, vector).items.arr
