91590ed149ac3f23b6c3150763763d566f608e2b
Maxim Gekk <gekk.maxim@huawei.com>
2015-07-28 16:34:21 +0300

    Generated boilerplate for wrappers.

--- src/main/scala/scalanizer/wrappers/ArrWrappers.scala
+++ src/main/scala/scalanizer/wrappers/ArrWrappers.scala
@@ -1,14 +1,24 @@
 package scalanizer.wrappers
 
 import scalan._
+import scalan.common.Default
 import scalanizer.MyArr
+import impl._
 
 trait MyArrWrappers extends Base with TypeWrappers {self: WrappersDsl =>
+  type RepMyArr[T] = Rep[MyArrWrapper[T]]
+
   trait MyArrWrapper[T] extends TypeWrapper[MyArr[T], MyArrWrapper[T]] {self =>
     def wrappedValueOfBaseType: Rep[MyArr[T]];
     implicit def eeT: Elem[T];
     @External def apply(j: Rep[Int]): Rep[T];
     @External def length: Rep[Int]
+  }
+
+  trait MyArrWrapperCompanion
+
+  def DefaultOfMyArr[T:Elem]: Default[MyArr[T]] = {
+    null
   }
 }
 
