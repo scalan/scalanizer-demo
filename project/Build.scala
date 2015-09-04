f6f45004d5b9296d6cdc4aa89e9f1b533a4c43a4
Maxim Gekk <gekk.maxim@huawei.com>
2015-09-04 10:18:15 +0300

    Prepared for standing after typer

--- project/Build.scala
+++ project/Build.scala
@@ -13,18 +13,18 @@ object ScalanParadiseRootBuild extends Build {
       "-language:existentials",
       "-language:postfixOps"
     ),
-    addCompilerPlugin("com.huawei" %% "scalanizer" % "0.0.2" classifier "assembly")
+    addCompilerPlugin("com.huawei" %% "scalanizer" % "0.0.3" classifier "assembly")
   )
 
   lazy val scalanizerSample = Project(
     id = "scalanizer-sample",
     base = file(".")
   ).settings(
-    buildSettings,
-    libraryDependencies ++= Seq(
-      "com.huawei.scalan" %% "scalan-lms-backend" % "0.2.11-SNAPSHOT",
-      "org.scalatest" %% "scalatest" % "2.2.1" % "test"
-    ),
-    scalaOrganization := "org.scala-lang.virtualized",
-    scalaVersion := "2.11.2")
+      buildSettings,
+      libraryDependencies ++= Seq(
+        "com.huawei.scalan" %% "scalan-lms-backend" % "0.2.9-SNAPSHOT",
+        "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+      ),
+      scalaOrganization := "org.scala-lang.virtualized",
+      scalaVersion := "2.11.2")
 }
--- src/main/scala/paradise/linalgebra/impl/LinearAlgebraImpl.scala
+++ src/main/scala/paradise/linalgebra/impl/LinearAlgebraImpl.scala
@@ -1,39 +1,39 @@
-package scalanizer.linalgebra {
-  package implOfLinearAlgebra {
-    object StagedEvaluation {
-
-      import scalan._
-      import scalanizer.implOfNums.StagedEvaluation._
-      import scalanizer.implOfNumMonoids.StagedEvaluation._
-      import scalanizer.collections.implOfCols.StagedEvaluation._
-      import scalanizer.linalgebra.implOfVecs.StagedEvaluation._
-      import scalanizer.linalgebra.implOfMatrs.StagedEvaluation._
-      import scalanizer.linalgebra.implOfLinearAlgebraOps.StagedEvaluation._
-
-      trait LinearAlgebraDsl extends Scalan
-      with NumsDsl
-      with NumMonoidsDsl
-      with ColsDsl
-      with VecsDsl
-      with MatrsDsl
-      with LinearAlgebraOpsDsl
-
-      trait LinearAlgebraDslSeq extends ScalanSeq with LinearAlgebraDsl
-      with NumsDslSeq
-      with NumMonoidsDslSeq
-      with ColsDslSeq
-      with VecsDslSeq
-      with MatrsDslSeq
-      with LinearAlgebraOpsDslSeq
-
-      trait LinearAlgebraDslExp extends ScalanExp with LinearAlgebraDsl
-      with NumsDslExp
-      with NumMonoidsDslExp
-      with ColsDslExp
-      with VecsDslExp
-      with MatrsDslExp
-      with LinearAlgebraOpsDslExp
-
-    }
-  }
-}
+//package scalanizer.linalgebra {
+//  package implOfLinearAlgebra {
+//    object StagedEvaluation {
+//
+//      import scalan._
+//      import scalanizer.implOfNums.StagedEvaluation._
+//      import scalanizer.implOfNumMonoids.StagedEvaluation._
+//      import scalanizer.collections.implOfCols.StagedEvaluation._
+//      import scalanizer.linalgebra.implOfVecs.StagedEvaluation._
+//      import scalanizer.linalgebra.implOfMatrs.StagedEvaluation._
+//      import scalanizer.linalgebra.implOfLinearAlgebraOps.StagedEvaluation._
+//
+//      trait LinearAlgebraDsl extends Scalan
+//      with NumsDsl
+//      with NumMonoidsDsl
+//      with ColsDsl
+//      with VecsDsl
+//      with MatrsDsl
+//      with LinearAlgebraOpsDsl
+//
+//      trait LinearAlgebraDslSeq extends ScalanSeq with LinearAlgebraDsl
+//      with NumsDslSeq
+//      with NumMonoidsDslSeq
+//      with ColsDslSeq
+//      with VecsDslSeq
+//      with MatrsDslSeq
+//      with LinearAlgebraOpsDslSeq
+//
+//      trait LinearAlgebraDslExp extends ScalanExp with LinearAlgebraDsl
+//      with NumsDslExp
+//      with NumMonoidsDslExp
+//      with ColsDslExp
+//      with VecsDslExp
+//      with MatrsDslExp
+//      with LinearAlgebraOpsDslExp
+//
+//    }
+//  }
+//}
